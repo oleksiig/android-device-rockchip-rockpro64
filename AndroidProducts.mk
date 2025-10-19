bd963c7a7c14f4098417723548d8251302998e61
Oleksii Gulchenko <alexey.gulchenko@gmail.com>
2025-10-19 11:42:03 +0200

    Android 16 support
    Signed-off-by: Oleksii Gulchenko <alexey.gulchenko@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The Android Open-Source Project
+# Copyright (C) 2025 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,5 +27,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/rockpro64.mk
 
 COMMON_LUNCH_CHOICES := \
-    rockpro64-userdebug \
-    rockpro64-user
+    rockpro64-trunk_staging-userdebug \
+    rockpro64-trunk_staging-user
